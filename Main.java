7658b1c0307fdad2c193af0e7473f82c642c2f2b
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-21 16:51:28 +0000

    Setup phrases

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import java.util.*;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    private static String[] PHRASE_LIST = new String[]{ "Beating a Dead Horse", "Roll With the Punches", "Down And Out","Dont Mine at Night","No Ifs Ands or Buts", "Raining Cats and Dogs", "Keep On Truckin", "Short End of the Stick","Like Father Like Son", "Elvis Has Left The Building"};
+    private array
   }
 }
