a2280969ac12baf085b922e9ec133a2dab5d7d98
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-21 17:06:40 +0000

    Setup arrays

--- Main.java
+++ Main.java
@@ -1,7 +1,29 @@
 import java.util.*;
+
 class Main {
-  public static void main(String[] args) {
-    private static String[] PHRASE_LIST = new String[]{ "Beating a Dead Horse", "Roll With the Punches", "Down And Out","Dont Mine at Night","No Ifs Ands or Buts", "Raining Cats and Dogs", "Keep On Truckin", "Short End of the Stick","Like Father Like Son", "Elvis Has Left The Building"};
-    private array
-  }
+    private static String[] PHRASE_LIST = new String[] { "Beating a Dead Horse", "Roll With the Punches","Down And Out", "Dont Mine at Night", "No Ifs Ands or Buts", "Raining Cats and Dogs", "Keep On Truckin","Short End of the Stick", "Like Father Like Son", "Elvis Has Left The Building" };
+
+    private ArrayList<String> guesses;
+    private String currentWord;
+    
+
+
+
+  private String pickWord() {
+    return (PHRASE_LIST[(int) (Math.random() * PHRASE_LIST.length)]);
+
 }
+  private String makeGuess(){
+    Scanner input = new Scanner(System.in);
+    String resultToDisplay = "";
+
+    
+  }
+
+
+}
+
+
+
+
+
