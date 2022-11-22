c2d4f413b76df73e0a016954313b1772a1efdbee
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-22 16:37:21 +0000

    Reset

--- Main.java
+++ Main.java
@@ -1,42 +1,20 @@
 import java.util.*;
 
 class Main {
-  // Create PHRASE_LIST String List/Array
+  // Create PHRASE_LIST String List
     private static String[] PHRASE_LIST = new String[] { "Beating a Dead Horse", "Roll With the Punches","Down And Out", "Dont Mine at Night", "No Ifs Ands or Buts", "Raining Cats and Dogs", "Keep On Truckin","Short End of the Stick", "Like Father Like Son", "Elvis Has Left The Building" };
 
-    private ArrayList<String> guesses;
-    private String currentPhrase;
-    
+  
 
-
-
-  private String pickPhrase() {
-    return (PHRASE_LIST[(int) (Math.random() * PHRASE_LIST.length)]);
-
-}
-  private String makeGuess(){
-    Scanner input = new Scanner(System.in);
-    System.out.println("What is your guess?");
-    String guess = input.nextLine();
-    System.out.println(guess);
-    
-    
-
-    ArrayList<Character> charachterList = new ArrayList<Character>();
-    ArrayList<Integer> guessesRemaining = new ArrayList<Integer>();
-    // Sequence through the phrase and check if the the charachter matches the phrase
-    for (int i = 0; i < currentPhrase.length(); i++){
-      char charachterCheck = currentPhrase.charAt(i);
-      int index = -1;
+  
+      
       
     }
-    
-
     
   }
 
 
-}
+
 
 
 
