6e279403c335032d644917a5667b1fd6553346bb
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-22 17:40:17 +0000

    Setup random and while loop

--- Main.java
+++ Main.java
@@ -1,17 +1,25 @@
 import java.util.*;
-
 class Main {
+  public static void main(String[] args) {
   // Create PHRASE_LIST String List
-    private static String[] PHRASE_LIST = new String[] { "Beating a Dead Horse", "Roll With the Punches","Down And Out", "Dont Mine at Night", "No Ifs Ands or Buts", "Raining Cats and Dogs", "Keep On Truckin","Short End of the Stick", "Like Father Like Son", "Elvis Has Left The Building" };
-
-  
-
-  
-      
-      
-    }
+  Scanner input = new Scanner(System.in);
+  String PHRASE_LIST[] = { "Beating a Dead Horse", "Roll With the Punches","Down And Out", "Dont Mine at Night", "No Ifs Ands or Buts", "Raining Cats and Dogs", "Keep On Truckin","Short End of the Stick", "Like Father Like Son", "Elvis Has Left The Building" };
+  System.out.println("What is your guess?");
+  String userInput = input.nextLine();
+  int guessesRemaining = 0;
+  int random = math.random * 100;
+  while(guessesRemaining < 6){
     
   }
+  
+
+  
+      
+      
+    
+    
+  }
+}
 
 
 
