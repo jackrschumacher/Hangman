4f9bdb0a7eaad7938013d11b315a6b74de8f33dc
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-26 20:12:38 +0000

    Setup Loop

--- Main.java
+++ Main.java
@@ -4,12 +4,14 @@ class Main {
   // Create PHRASE_LIST String List
   Scanner input = new Scanner(System.in);
   String PHRASE_LIST[] = { "Beating a Dead Horse", "Roll With the Punches","Down And Out", "Dont Mine at Night", "No Ifs Ands or Buts", "Raining Cats and Dogs", "Keep On Truckin","Short End of the Stick", "Like Father Like Son", "Elvis Has Left The Building" };
-  System.out.println("What is your guess?");
-  String userInput = input.nextLine();
+  
   int guessesRemaining = 0;
   int random = math.random * 100;
   string randomWord = PHRASE_LIST[] * random;
   while(guessesRemaining < 6){
+    System.out.println("What is your guess?");
+    String userInput = input.nextLine();
+    String USER_INPUTS[] = userInput
     for(int i = 0; i < randomWord.length(); i++){
       char charachterCheck = randomWord.charAt(i);
       int index = -1;
@@ -17,6 +19,9 @@ class Main {
       if(charachterCheck == randomWord.get(j)){
         index = j;
       }
+  if(guessesRemaining == 0){
+    System.out.println("You Lose!");
+  }
       
     }
     }
