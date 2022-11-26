155ad60453ed4112ae81c47fb86b841f9ea595c3
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-11-26 20:08:42 +0000

    Setup for Loop

--- Main.java
+++ Main.java
@@ -8,7 +8,19 @@ class Main {
   String userInput = input.nextLine();
   int guessesRemaining = 0;
   int random = math.random * 100;
+  string randomWord = PHRASE_LIST[] * random;
   while(guessesRemaining < 6){
+    for(int i = 0; i < randomWord.length(); i++){
+      char charachterCheck = randomWord.charAt(i);
+      int index = -1;
+    for(int j = 0; j < randomWord.size(); j++){
+      if(charachterCheck == randomWord.get(j)){
+        index = j;
+      }
+      
+    }
+    }
+    
     
   }
   
