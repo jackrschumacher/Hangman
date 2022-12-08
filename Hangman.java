c928430002c817696c7ced82cfcbbdc8fd46c040
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-12-08 04:19:52 +0000

    Bug Fixes

--- Hangman.java
+++ Hangman.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.lang.*;
 
 public class Hangman {
+	// Main
 	// Setup private Array List PHRASE_LIST
 	private static String[] PHRASE_LIST = new String[] { "Beating a Dead Horse", "Roll With the Punches", "Down And Out",
 			"Dont Mine at Night", "No Ifs Ands or Buts", "Raining Cats and Dogs", "Keep On Truckin", "Short End of the Stick",
@@ -16,13 +17,12 @@ public class Hangman {
 	public ArrayList<Character> currentPhrase;
 
 	public Hangman() {
-
+		System.out.println("Hello!");
 		currentPhraseStr = selectPhrase();
 		currentPhrase = new ArrayList<Character>();
 		guessedLetters = new ArrayList<Character>();
 		guessedPhrase = new ArrayList<Character>();
 		setupWord(currentPhraseStr);
-		getLetter();
 		playGame();
 	}
 
@@ -35,38 +35,29 @@ public class Hangman {
 		// guessed and current phrase
 		for (int i = 0; i < phrase.length(); i++) {
 			System.out.println(phrase.charAt(i));
-			currentPhrase.set(i, phrase.charAt(i));
+			currentPhrase.add(phrase.charAt(i).toUpperCase());
 			if (phrase.charAt(i) == ' ') {
-				guessedPhrase.set(i, ' ');
+				guessedPhrase.add(' ');
 			} else {
-				guessedPhrase.set(i, '_');
+				guessedPhrase.add('_');
 			}
 
 		}
-		System.out.println(guessedPhrase.get(1));
+
+		System.out.println("Guessed Phrase:" + guessedPhrase.get(1));
 		System.out.println(currentPhrase.get(1));
 		return;
 	}
 
-	private void getLetter() {
+	private char getLetter() {
 		int guessesRemaing = 0;
 		Scanner input = new Scanner(System.in);
 		Character guess = ' ';
-		for (int guessesRemaining = 0; guessesRemaining < 6; guessesRemaining++) {
-			System.out.println("Please Enter a Letter that you wish to Guess");
-			guess = input.next().charAt(0);
-			guess = Character.toLowerCase(guess);
-			// If guessed Letter is equal to guessedLetters list (list of all the letters that the user has inputted) at the index of the current guess 
-			// Search for letter in the PHRASE_LIST
-			boolean foundLetter = findLetter(guess);
-			
-			if (foundLetter) {
-				System.out.println("You found a new letter!" + guess);
-			} 
-			else {
-				System.out.println("You do not find a letter that matches.");
-			}
-		}
+		System.out.println("Please Enter a Letter that you wish to Guess");
+		guess = input.next().charAt(0);
+		guess = Character.toUpperCase(guess);
+		
+		
 
 		// Get the guessed letter DONE
 		// Check to make sure that it already has not been guessed LATER
@@ -74,21 +65,18 @@ public class Hangman {
 		// numbers LATER? - Does java do this w/ out of bounds errors?
 		// To Do:
 		// Do loop with input until you get a valid guess
-		return;
+		return guess;
 	}
 
 	private boolean findLetter(char letter) {
 		// Increment throught the array list, checking at each index if the letter guessed by the user matches
 		boolean found = false;
-		for (int i = 0; i <= currentPhrase.size(); i++) {
+		for (int i = 0; i <= currentPhrase.size()-1; i++) {
 			if (letter == currentPhrase.get(i)) {
-				System.out.println("This is a letter in the phrase.");
+				// This is a letter in the phrase
+				guessedPhrase.set(i, letter);
 				found = true;
 			} 
-			else {
-				System.out.println("This is not a letter in the phrase.");
-			}
-
 		}
 		return found;
 	}
@@ -99,11 +87,26 @@ public class Hangman {
 	// taken, convert all to Upppercase
 
 	public void playGame() {
-		// int guessesRemaining = 0;
-		// while(guessesRemaining < 6){
 		// System.out.println("Please enter a letter that you would like to find in the
 		// phrase.");
+			// If guessed Letter is equal to guessedLetters list (list of all the letters that the user has inputted) at the index of the current guess 
+			// Search for letter in the PHRASE_LIST
+			
+		int guessesRemaining = 6;
+		char guess;
+		boolean foundLetter;
 		
+		while(guessesRemaining > 0){
+			guess = getLetter();
+			foundLetter = findLetter(guess);
+			if(foundLetter){
+				System.out.println("You found the Letter: " + guess);
+			}
+			else{
+				System.out.println("You did not find the Letter: " + guess);
+			}
+			
 	}
 
+}
 }
